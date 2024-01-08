d5e593adf1a7c72e83984ef03141258e2c55b612
Yuval Shavit <yuvals@wix.com>
2024-01-08 15:45:08 +0200

    add files

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.spreadsheet.client","l":"SpreadsheetClient"},{"p":"com.spreadsheet.client","l":"SpreadsheetOperations"}];updateSearchResults();
